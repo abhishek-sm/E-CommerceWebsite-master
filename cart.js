5cbb335ec9384b278d7201942b0c64ae2d6e1708
Rajesh S <rajesh.s@7edge.com>
2024-10-01 15:27:28 +0530

    done

--- cart.js
+++ cart.js
@@ -135,7 +135,7 @@ httpRequest.onreadystatechange = function()
         }
 }
 
-httpRequest.open('GET', 'https://5d76bf96515d1a0014085cf9.mockapi.io/product', true)
+httpRequest.open('GET', 'https://66fbba9f8583ac93b40cf38f.mockapi.io/hairoil/v1/product/', true)
 httpRequest.send()
 
 
