c67230ec4a5e4fa3ae44628270ab2563083869f1
Rajesh S <rajesh.s@7edge.com>
2024-10-03 15:50:50 +0530

    changed css for product

--- content.js
+++ content.js
@@ -24,9 +24,9 @@ function dynamicClothingSection(ob) {
   let h3Text = document.createTextNode(ob.name);
   h3.appendChild(h3Text);
 
-  let h4 = document.createElement("h4");
-  let h4Text = document.createTextNode(ob.brand);
-  h4.appendChild(h4Text);
+  // let h4 = document.createElement("h4");
+  // let h4Text = document.createTextNode(ob.brand);
+  // h4.appendChild(h4Text);
 
   let h2 = document.createElement("h2");
   let h2Text = document.createTextNode("rs  " + ob.price);
@@ -36,7 +36,7 @@ function dynamicClothingSection(ob) {
   boxLink.appendChild(imgTag);
   boxLink.appendChild(detailsDiv);
   detailsDiv.appendChild(h3);
-  detailsDiv.appendChild(h4);
+  // detailsDiv.appendChild(h4);
   detailsDiv.appendChild(h2);
 
   return boxDiv;
